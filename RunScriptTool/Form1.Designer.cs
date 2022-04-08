399d044111d7ded038d160d21d37fb9b9d3b364a
Huy Dat <dat.nguyen4@ntq-solution.com.vn>
2022-04-08 14:08:36 +0700

    Add project files.

---
+++ RunScriptTool/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace RunScriptTool
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.txtFolder = new System.Windows.Forms.TextBox();
+			this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+			this.txtConnString = new System.Windows.Forms.TextBox();
+			this.btnBrowse = new System.Windows.Forms.Button();
+			this.btnRun = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// txtFolder
+			// 
+			this.txtFolder.Location = new System.Drawing.Point(98, 79);
+			this.txtFolder.Name = "txtFolder";
+			this.txtFolder.Size = new System.Drawing.Size(433, 20);
+			this.txtFolder.TabIndex = 0;
+			this.txtFolder.Text = "D:\\Weblite\\docs\\SqlScripts";
+			// 
+			// txtConnString
+			// 
+			this.txtConnString.Location = new System.Drawing.Point(98, 105);
+			this.txtConnString.Name = "txtConnString";
+			this.txtConnString.Size = new System.Drawing.Size(514, 20);
+			this.txtConnString.TabIndex = 1;
+			this.txtConnString.Text = "DATA SOURCE=localhost:1521/XE;PASSWORD=ixfds;PERSIST SECURITY INFO=True;USER ID=I" +
+    "XFDS";
+			// 
+			// btnBrowse
+			// 
+			this.btnBrowse.Location = new System.Drawing.Point(537, 77);
+			this.btnBrowse.Name = "btnBrowse";
+			this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+			this.btnBrowse.TabIndex = 2;
+			this.btnBrowse.Text = "Browse";
+			this.btnBrowse.UseVisualStyleBackColor = true;
+			// 
+			// btnRun
+			// 
+			this.btnRun.Location = new System.Drawing.Point(537, 131);
+			this.btnRun.Name = "btnRun";
+			this.btnRun.Size = new System.Drawing.Size(75, 23);
+			this.btnRun.TabIndex = 3;
+			this.btnRun.Text = "Run Scripts";
+			this.btnRun.UseVisualStyleBackColor = true;
+			this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(57, 82);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(35, 13);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "label1";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(57, 108);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(35, 13);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "label2";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(746, 214);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.btnRun);
+			this.Controls.Add(this.btnBrowse);
+			this.Controls.Add(this.txtConnString);
+			this.Controls.Add(this.txtFolder);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox txtFolder;
+		private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+		private System.Windows.Forms.TextBox txtConnString;
+		private System.Windows.Forms.Button btnBrowse;
+		private System.Windows.Forms.Button btnRun;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+	}
+}
+
---
+++ RunScriptTool/Form1.cs
@@ -0,0 +1,82 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RunScriptTool
+{
+	public partial class Form1 : Form
+	{
+		private string m_ConnString = string.Empty;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void btnRun_Click(object sender, EventArgs e)
+		{
+			string folder = txtFolder.Text;
+			if (!Directory.Exists(folder))
+			{
+				MessageBox.Show("Ko ton tai folder");
+			}
+
+			//List *.sql Files
+			string[] files = Directory.GetFiles(folder, "*.sql", SearchOption.AllDirectories);
+
+			//Connection String
+			m_ConnString = txtConnString.Text;
+			for (int i = 0; i < files.Length; i++)
+			{
+				try
+				{
+					//Run Script to Create/Update/Delete TABLE in Oracle
+					var errMsg = RunScripts(files[i]);
+					if (!string.IsNullOrWhiteSpace(errMsg))
+					{
+						MessageBox.Show(errMsg);
+					}
+				}
+				catch (Exception ex)
+				{
+					Debug.WriteLine(ex);
+					MessageBox.Show(ex.ToString());
+					//throw;
+				}
+			}
+			MessageBox.Show("Done");
+		}
+
+		private string RunScripts(string filePath)
+		{
+			string[] fileContent = File.ReadAllLines(filePath, Encoding.UTF8);
+			string sql = GetScript(fileContent);
+			using (OracleConnection objConn = new OracleConnection(m_ConnString))
+			{
+				OracleCommand objCmd = new OracleCommand();
+				objCmd.Connection = objConn;
+				objCmd.CommandText = sql;
+				objCmd.CommandType = CommandType.Text;
+				try
+				{
+					objConn.Open();
+					objCmd.ExecuteNonQuery();
+				}
+				catch (Exception ex)
+				{
+					return ex.ToString();
+				}
+			}
+			return string.Empty;
+		}
+
+		private string GetScript(string[] fileContent)
+		{
+			return "CREATE TABLE \"FL_PRINTING_COMPANY_INFO\"(\"SID\" NUMBER(10,0) \"PRINTING_COMPANY_NAME\" VARCHAR2(100 CHAR), \"INVALIDATED\" NUMBER(1,0),\"UPDATE_USER_SID\" NUMBER(8,0),\"ENTRY_DATE\" DATE,	\"UPDATE_DATE\" DATE);";
+		}
+	}
+}
